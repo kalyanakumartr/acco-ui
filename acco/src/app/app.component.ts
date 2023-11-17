6aa74c9af03fa06dab9ac1bebe55cf7a917c5179
muthuslm2006 <muthuslm2006@gmail.com>
2023-11-17 22:53:26 +0530

    sample valid in app component

--- acco/src/app/app.component.ts
+++ acco/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  formdata={fname:""};
+  submit=false;
+
   title = 'acco';
+
+  onsubmit()
+  {
+
+  }
 }
