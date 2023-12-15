23b70672627f34aa75c73cb02048f5db0286efa3
ananthee89 <ananthee89@gmail.com>
2023-12-15 21:18:09 +0530

    changes roomtype component
    changes roomtype component

--- acco/src/app/component/roomtype/roomtype.component.ts
+++ acco/src/app/component/roomtype/roomtype.component.ts
@@ -11,158 +11,172 @@ import { GetroomlistService } from 'src/app/services/getroomlist.service';
 export class RoomtypeComponent implements OnInit {
   //  @Input() fd: any;
 
-  roomData:any;
-  chargedData:any;
+  roomData: any;
+  chargedData: any;
   days: any;
-  adult:any;
-  cIn:any;
-  cOut:any;
+  adult: any;
+  cIn: any;
+  cOut: any;
   selectedRow: any;
-  child:any;
-  roomtype:any;
-selectedAll: boolean = false;
+  child: any;
+  roomtype: any;
+  selectedAll: boolean = false;
+
+  roomid: Array<any> = [];
+  roomId: Array<any> = []
+  checkedList: any[];
+  currentSelected: {} | undefined;
+  showDropDown!: boolean;
 
   constructor(
-    private getroomlistservice:GetroomlistService,
-    private getChargedAmenity:GetchargedamenityService,
+    private getroomlistservice: GetroomlistService,
+    private getChargedAmenity: GetchargedamenityService,
     private homeroute: ActivatedRoute,
     private router: Router,
 
-    ){
-      this.selectedRow = [];
+  ) {
+    this.selectedRow = [];
     getroomlistservice.apiRoom$.subscribe(data => this.roomData = data)
+    this.checkedList = [];
+    this.roomid = [
+      this.roomData[0].roomids,
+    ],
+      this.roomId = [
+        this.roomData[1].roomids
+      ]
   }
   ngOnInit(): void {
-   console.log("room:",this.roomData) ;
-   this.getChargedAmenities();
-   
+    console.log("room:", this.roomData);
+    this.getChargedAmenities();
+
     this.homeroute.params.subscribe((params: Params) =>
-     this.days = params[('days')], );    
-     this.homeroute.params.subscribe((params: Params) =>
-     this.adult = params[('adult')], );
-     this.homeroute.params.subscribe((params: Params) =>
-     this.cIn = params[('cIn')], );
-     this.homeroute.params.subscribe((params: Params) =>
-     this.cOut = params[('cOut')], );
-     this.homeroute.params.subscribe((params: Params) =>
-     this.child = params[('child')], );
-     this.homeroute.params.subscribe((params: Params) =>
-     this.roomtype = params[('roomType')], );
-     
-    console.log("days:",this.days)
-    console.log("adult:",this.adult)
-    console.log("cIn:",this.cIn)
-    console.log("cOut:",this.cOut)
-    console.log("child:",this.child)
-    console.log("roomtype:",this.roomtype)
-    
+      this.days = params[('days')],);
+    this.homeroute.params.subscribe((params: Params) =>
+      this.adult = params[('adult')],);
+    this.homeroute.params.subscribe((params: Params) =>
+      this.cIn = params[('cIn')],);
+    this.homeroute.params.subscribe((params: Params) =>
+      this.cOut = params[('cOut')],);
+    this.homeroute.params.subscribe((params: Params) =>
+      this.child = params[('child')],);
+    this.homeroute.params.subscribe((params: Params) =>
+      this.roomtype = params[('roomType')],);
+
+    console.log("days:", this.days)
+    console.log("adult:", this.adult)
+    console.log("cIn:", this.cIn)
+    console.log("cOut:", this.cOut)
+    console.log("child:", this.child)
+    console.log("roomtype:", this.roomtype)
+
+    console.log("roomids:", this.roomData[0].roomids);
+    console.log("roomids1:", this.roomData[1].roomids);
+
   }
   inputnumber = 0;
   number = 0;
   bednumber = 0;
   foodnumber = 0;
 
-  getChargedAmenities(){
-  this.getChargedAmenity.getChargedData().subscribe((res)=>{
-    console.log(res);
-    this.chargedData=res;
-            console.log("++++chargedData:",this.chargedData);
-    
-  });
-}
+  getChargedAmenities() {
+    this.getChargedAmenity.getChargedData().subscribe((res) => {
+      console.log(res);
+      this.chargedData = res;
+      console.log("++++chargedData:", this.chargedData);
 
-
-selectAll(index: any) {
-  if (typeof (index) == 'undefined') {
-   this.selectedAll = !this.selectedAll;
-   this.selectedRow = [];
-  } else {
-   this.selectedRow.push(index);
-   console.log("rooooom",this.selectedRow);
+    });
   }
- }
 
-sendbookeddata(data:any){
-console.log(data)
- this.router.navigate(["bookingsummary",{"id":data.roomid,
- "name":data.roomname,
- "price":data.price,
- "nodays":this.days,
- "checkIn":this.cIn,
- "checkOut":this.cOut,
-"adult":this.adult,
-"child":this.child,
-"roomtype":this.roomtype,
-}]);
 
-}
-  
-  plus()
-  {
-    if(this.inputnumber<6){
-    
-   this.inputnumber = this.inputnumber+1;
+  selectAll(index: any) {
+    if (typeof (index) == 'undefined') {
+      this.selectedAll = !this.selectedAll;
+      this.selectedRow = [];
+    } else {
+      this.selectedRow.push(index);
+      console.log("rooooom", this.selectedRow);
     }
   }
-  plus1()
-  {
-    if(this.number<6){
-      this.number = this.number+1;
-    }
-    
-  }
-  plus2()
-  {
-   
-    if(this.bednumber<6){
-      this.bednumber = this.bednumber+1;
-    }
-  }
-    plus3()
-    {
-      if(this.foodnumber<12){
-        this.foodnumber = this.foodnumber+1;
-      }
-    }
 
-    minus()
-  {
-    if(this.inputnumber != 0)
-  {
-   this.inputnumber = this.inputnumber-1;
-  }
-  
-  
-  }
-  minus1()
-  {
-    if(this.number !=0) 
-  {
-    this.number = this.number-1;
-  }
-  
+  sendbookeddata(data: any) {
+    console.log(data)
+    this.router.navigate(["bookingsummary", {
+      "id": data.roomid,
+      "name": data.roomname,
+      "price": data.price,
+      "nodays": this.days,
+      "checkIn": this.cIn,
+      "checkOut": this.cOut,
+      "adult": this.adult,
+      "child": this.child,
+      "roomtype": this.roomtype,
+    }]);
 
   }
-  minus2()
-  {
-    
-  if(this.bednumber !=0)
-  {
-    this.bednumber = this.bednumber-1;
-  }
 
-  }
-  minus3()
-  {
-    if(this.foodnumber !=0)
-    {
-      this.foodnumber = this.foodnumber-1;
+  plus() {
+    if (this.inputnumber < 6) {
+
+      this.inputnumber = this.inputnumber + 1;
     }
   }
-  
+  plus1() {
+    if (this.number < 6) {
+      this.number = this.number + 1;
+    }
 
-  
-  
+  }
+  plus2() {
+
+    if (this.bednumber < 6) {
+      this.bednumber = this.bednumber + 1;
+    }
+  }
+  plus3() {
+    if (this.foodnumber < 12) {
+      this.foodnumber = this.foodnumber + 1;
+    }
+  }
+
+  minus() {
+    if (this.inputnumber != 0) {
+      this.inputnumber = this.inputnumber - 1;
+    }
+
+
+  }
+  minus1() {
+    if (this.number != 0) {
+      this.number = this.number - 1;
+    }
+
+
+  }
+  minus2() {
+
+    if (this.bednumber != 0) {
+      this.bednumber = this.bednumber - 1;
+    }
+
+  }
+  minus3() {
+    if (this.foodnumber != 0) {
+      this.foodnumber = this.foodnumber - 1;
+    }
+  }
+
+  getSelectedValue(status: Boolean, value: String) {
+    if (status) {
+      this.checkedList.push(value);
+    } else {
+      var index = this.checkedList.indexOf(value);
+      this.checkedList.splice(index, 1);
+    }
+
+    this.currentSelected = { checked: status, name: value };
+  }
+
+
 
 
 
