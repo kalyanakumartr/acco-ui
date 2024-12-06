6ee144b4b2a2088ec5be279a65f1222e50bcdd6f
Anupama1306 <whizantanu4@gmail.com>
2024-12-06 12:47:57 +0530

    date changes
    date changes

--- acco/src/app/component/home/home.component.ts
+++ acco/src/app/component/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit ,ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, MaxLengthValidator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GetRoomList } from 'src/app/model/getroomlist.model';
@@ -45,9 +45,16 @@ export class HomeComponent implements OnInit {
   isDisabled: boolean = false;
   isDropdownOpen: boolean = false;
   selectedDate: Date = new Date();
-  // selectedTime: Date | null = null;
   times: string[] = [];
   selectedTime: any;
+  finalmonth: any;
+  finalday: any;
+  finalOutday: any;
+  tokenvalue: any;
+  visibleRoom: any;
+  roomBooking: any;
+
+  
   constructor(private fb: FormBuilder, private http: HttpClient,
     private router: Router, private getroomlistservice: GetroomlistService,
     private roomTypeService: GetroomtypeService,
@@ -55,49 +62,15 @@ export class HomeComponent implements OnInit {
     private cdr: ChangeDetectorRef
   ) { }
 
-  date1 = new Date();
-  currentyear = this.date1.getUTCFullYear();
-  currentmonth = this.date1.getUTCMonth() + 1;
-  currentday = this.date1.getUTCDate();
-  checkoutday = this.date1.getDate() + 1;
-  currentmin = this.date1.getMinutes();
-  currenthour = this.date1.getHours();
-
-  finalmonth: any;
-  finalday: any;
-  finalOutday: any;
-  tokenvalue: any;
-  visibleRoom: any;
-  roomBooking: any;
+  
   ngOnInit(): void {
-
+    const date1 = new Date();
     this.generateTimeIntervals();
     this.setCurrentTime();
-
     this.showRoomType();
+    this.setCheckInOut(date1);
     console.log("datenow", this.selectedDate)
     this.tokenvalue = localStorage.getItem('token');
-    if (this.currentmonth < 10) {
-      this.finalmonth = "0" + this.currentmonth;
-    } else {
-      this.finalmonth = this.currentmonth;
-    }
-    if (this.currentday < 10) {
-      this.finalday = "0" + this.currentday;
-    } else {
-      this.finalday = this.currentday;
-    }
-    if (this.checkoutday < 10) {
-      this.finalOutday = "0" + this.checkoutday;
-    } else {
-      this.finalOutday = this.checkoutday;
-    }
-
-    this.Todaydate = this.currentyear + "-" + this.finalmonth + "-" + this.finalday
-    //  + " " + this.currenthour + ":" + this.currentmin;
-    this.outDate = this.currentyear + "-" + this.finalmonth + "-" + this.finalOutday
-    // + " " + this.currenthour + ":" + this.currentmin;
-    // this.selectedTime= this.currenthour + ":" + this.currentmin;
 
     this.homeForm = this.fb.group({
       checkIn: ['', Validators.required],
@@ -108,36 +81,51 @@ export class HomeComponent implements OnInit {
       child: ['0', [Validators.required, Validators.max(6)]],
       roomType: ['1', Validators.required],
     })
+  }
 
+  setCheckInOut(date1:Date){
+    console.log('1111',date1)
 
+   const currentyear = date1.getUTCFullYear();
+   const currentmonth = date1.getUTCMonth() + 1;
+   const currentday = date1.getUTCDate();
+   const checkoutday = date1.getDate() + 1;
+   const currentmin = date1.getMinutes();
+   const currenthour = date1.getHours();
+   console.log('1111232',currentyear,currentmonth,currentday,checkoutday);
 
-    // for(let i=1;i<=9;i++){
-    //   this.childAge.push(i);
-    // }
-    // console.log("age:",this.childAge)
-    //   this.modalForm = this.fb.group({
-    // user: ['',], });
-    // this.getFloor();
-    // this.getroom();
+    if (currentmonth < 10) {
+      this.finalmonth = "0" + currentmonth;
+    } else {
+      this.finalmonth = currentmonth;
+    }
+    if (currentday < 10) {
+      this.finalday = "0" + currentday;
+    } else {
+      this.finalday = currentday;
+    }
+    if (checkoutday < 10) {
+      this.finalOutday = "0" + checkoutday;
+    } else {
+      this.finalOutday = checkoutday;
+    }
+    
+    this.Todaydate = currentyear + "-" + this.finalmonth + "-" + this.finalday
+    this.outDate = currentyear + "-" + this.finalmonth + "-" + this.finalOutday
+  
+  }
 
+  getNextDate(date: string) {
+    // Parse the date string to a Date object
+    const currentDate = new Date(date);
+
+    // Add one day (in milliseconds)
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    // Format the next date in YYYY-MM-DD format
+    this.outDate = currentDate.toISOString().split('T')[0];
 
   }
-  // modalProcess(){
-  //   this.selectedValue = this.modalForm.controls['user'].value;
-  //   console.log(this.selectedValue);    
-  //   if(this.selectedValue == 'NewUser'){
-  //     console.log(this.selectedValue);          
-  //            this.router.navigate(["signup"]);             
-  //   }else if(this.selectedValue=='ExistingUser'){
-  //     console.log(this.selectedValue);     
-  //     this.router.navigate(["login"]);
-
-  //   }
-  //   else{
-  //     Swal.fire("Please select value");
-  //   }
-  // }
-
 
   ReadMore: boolean = true
 
@@ -149,7 +137,6 @@ export class HomeComponent implements OnInit {
 
 
   ValueEntered(value: any) {
-    // console.log("djhckjkasj")
     this.samArray = []
     for (let i = 1; i <= value; i++) {
       this.samArray.push(i)
@@ -159,27 +146,15 @@ export class HomeComponent implements OnInit {
 
   //onclick toggling both
   clickon() {
-    //not equal to condition
     this.visible = !this.visible
   }
 
   childAgeMap = new Map<string, string>;
   ageValue: any;
+
   onSelected(value: any, e: any) {
     this.childAgeMap.set(e, value);
-
-    //   this.childMap.forEach((value: string, key: string) => {
-    //     console.log("++++",key, value);})
-    //   for ( let value of this.childMap.values()) {
-    //     console.log("aa:",value);
-    // }
-
     console.log("childage:", this.childAgeMap);
-
-    // this.childMap.forEach((value: string, key: string) => {
-    //       console.log("++++",key, value);
-    //       this.selectedAge[key]=value;
-    //     })
     const arr = [Array.from(this.childAgeMap).reduce((acc, curr) => ({
       ...acc,
       [curr[0]]: curr[1]
@@ -189,97 +164,36 @@ export class HomeComponent implements OnInit {
     this.ageValue = Object.values(arr[0]);
     console.log("age++", this.ageValue)
 
-    // this.selectedAge.push(value)
-    //  console.log("age:", this.selectedAge);
-
   }
 
-
-  // FirstFloor: boolean = true;
-  // FirstFloor1: boolean = true;
-  // FirstFloor2: boolean = true;
-  // ontouch() {
-  //   this.facilities = !this.facilities
-  // }
-
-  // toggleFirstFloor()
-  // {
-  //   this.FirstFloor = !this.FirstFloor;
-  // }
-
-  // toggleFirstFloor1()
-  // {
-  //   this.FirstFloor1 = !this.FirstFloor1;
-  // }
-
-
-  // toggleFirstFloor2()
-  // {
-  //   this.FirstFloor2 = !this.FirstFloor2;
-  // }
-
-  // plus() {
-  //   if (this.inputnumber < 6) {
-  //     this.inputnumber = this.inputnumber + 1;
-  //   }
-  // }
-  // minus() {
-  //   if (this.inputnumber != 0) {
-  //     this.inputnumber = this.inputnumber - 1;
-  //   }
-
-  // }
-  // foodnumber = 0;
-  // plus3() {
-  //   if (this.foodnumber < 12) {
-  //     this.foodnumber = this.foodnumber + 1;
-  //   }
-  // }
-  // minus3() {
-  //   if (this.foodnumber != 0) {
-  //     this.foodnumber = this.foodnumber - 1;
-  //   }
-  // }
-
-
   checkAvailability() {
-    console.log("formdata",this.homeForm.value);
+    console.log("formdata", this.homeForm.value);
     if (this.tokenvalue == null) {
-
       Swal.fire({
         text:
           " Please LOGIN if you are Existing user or SIGNUP for Newuser",
-        // "<h5 style='color:red'>"++"</h5>"
         confirmButtonColor: '#964B00',
         background: '#efc96a',
-      }).then((result)=>{
-        if(result.value){
+      }).then((result) => {
+        if (result.value) {
           this.router.navigate(["/login"])
         }
-
       })
     } else {
       const formData = this.homeForm.value;
       console.log("chlid:", formData.child, formData.roomType, formData.checkIn, formData.checkOut)
-      var checkingIn=`${formData.checkIn} ${formData.checkInTime}`;
-      var checkingOut=`${formData.checkOut} ${formData.checkOutTime}`;
+      var checkingIn = `${formData.checkIn} ${formData.checkInTime}`;
+      var checkingOut = `${formData.checkOut} ${formData.checkOutTime}`;
 
       var inDate = new Date(checkingIn);
       var OutDate = new Date(checkingOut);
-
-      // var noofdays = (OutDate.getTime() - inDate.getTime()) / (1000 * 3600 * 24);
-
-      // var noofdays = (OutDate.getTime() - inDate.getTime()) / (1000 * 3600 * 24);
-
-      // console.log("nnnnn", noofdays);
-      // console.log("nnnnn", formData.checkIn);
       var diff = OutDate.getTime() - inDate.getTime();
       var days = Math.floor(diff / (60 * 60 * 24 * 1000));
       var hours = Math.floor(diff / (60 * 60 * 1000)) - (days * 24);
       console.log("diff", diff);
       console.log("days", days);
       console.log("hours", hours);
-      console.log("checkincheckout",checkingIn,checkingOut);
+      console.log("checkincheckout", checkingIn, checkingOut);
       if (hours > 2) {
         var totalDays = days + 1
       } else {
@@ -293,22 +207,8 @@ export class HomeComponent implements OnInit {
           // "<h5 style='color:red'>"++"</h5>"
           confirmButtonColor: '#964B00',
           background: '#efc96a',
-          // confirmButtonText:'login',
-
-          
-          
         })
-
-      //  .then((result:any) => {
-      //           console.log('navigate check')
-      //       if (result.isConfirmed) {
-      //         // Use your navigation logic here
-      //         // window.location.href = '/login'; // Replace '/login' with your desired route
-      //          this.router.navigate(["login"]); 
-      //        }})
-        // this.router.navigate(["login"]);
       } else {
-        // this.getroomlistservice.roomlist(formData.adult, formData.checkIn, formData.checkIn, formData.roomType).subscribe((res) => {
         this.getroomlistservice.roomlogic(formData.adult, checkingIn, checkingOut, formData.roomType).subscribe((result) => {
           console.log(result);
           this.roomData = result[0];
@@ -323,7 +223,6 @@ export class HomeComponent implements OnInit {
               text: "We are Sorry! currently all rooms are occupied ",
             });
           } else {
-
             this.roomBooking = new BookingModel();
             this.roomBooking.checkin = checkingIn,
               this.roomBooking.checkout = checkingOut,
@@ -333,22 +232,10 @@ export class HomeComponent implements OnInit {
             this.roomBooking.childage = this.ageValue == undefined ? 0 : this.ageValue;
             this.roomBooking.roomtypeid = formData.roomType;
             this.roomBooking.modeoftypeid = 1;
-
-
             console.log("___+++", this.roomBooking)
             this.bookingService.changeMessage(this.roomBooking);
             this.router.navigate(["roomlogic",
-              // {
-              //   "days": noofdays,
-              //   "adult": formData.adult,
-              //   "cIn": formData.checkIn,
-              //   "cOut": formData.checkOut,
-              //   "child": formData.child,
-              //   "roomType": formData.roomType,
-              //   "childAge": this.ageValue
-              // }
             ]);
-
           }
         });
 
@@ -356,22 +243,22 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  getCheckOut() {
+    const checkinDate = this.homeForm.get('checkIn')?.value;
+    console.log("checkoutdate:",checkinDate );
+    this.getNextDate(checkinDate);
+  }
+
   showRoomType() {
-
-    this.roomTypeService.getRoomType(    )
-      // .subscribe((res)=>{
-
-      .subscribe((result) => { 
+    this.roomTypeService.getRoomType()
+      .subscribe((result) => {
         // this.cdr.detectChanges() 
         console.log("roomtype:", result);
         this.visibleRoom = result;
         console.log(this.visibleRoom);
       });
-      // this.cdr.detectChanges()
-
+    // this.cdr.detectChanges()
   }
-
-
 
   generateTimeIntervals() {
     const intervals: string[] = [];
@@ -384,9 +271,9 @@ export class HomeComponent implements OnInit {
       const minutes = i % 60;
 
       const formattedHours = hours.toString().padStart(2, '0');
-    const formattedMinutes = minutes.toString().padStart(2, '0');
+      const formattedMinutes = minutes.toString().padStart(2, '0');
 
-    intervals.push(`${formattedHours}:${formattedMinutes}`);
+      intervals.push(`${formattedHours}:${formattedMinutes}`);
     }
 
     this.times = intervals; // Assign to times array
@@ -401,11 +288,8 @@ export class HomeComponent implements OnInit {
 
     const adjustedHours = roundedMinutes === 60 ? (hours + 1) % 24 : hours;
     const formattedMinutes = roundedMinutes === 60 ? '00' : roundedMinutes.toString().padStart(2, '0');
-  
     // Format time in 24-hour format
     const formattedTime = `${adjustedHours.toString().padStart(2, '0')}:${formattedMinutes}`;
-  
-
     this.selectedTime = formattedTime;
     console.log('time now', this.selectedTime)
   }
